3ddf62af0357800899b26360f3204868fecbd50a
Ulrika Karlsson <ulrika.karlsson@yahoo.se>
2020-11-05 09:51:30 +0100

    Add project files.

---
+++ ObjectExercise_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectExercise_1
+{
+    class Program
+    {
+        class Person
+        {
+            public string firstName = "", lastName = "";
+        }
+        static void Main(string[] args)
+        {
+            Person P = new Person() 
+            { 
+                firstName = "Kalle",
+                lastName = "Anka" 
+            };
+            Person Q = new Person()
+            {
+                firstName = "Kajsa",
+                lastName = "Anka"
+            };
+
+        }
+    }
+}
